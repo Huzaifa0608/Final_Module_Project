789345e945ce578be89676c746040612a67a1387
“Huzaifa0608” <huzaifafarhan7890@gmail.com>
2023-07-06 10:15:59 +0500

    Work complete

---
+++ men.js
@@ -0,0 +1,98 @@
+function change(){
+
+    var a=document.getElementById("img1")
+    a.src="images/watch1.png"
+    
+}
+function change1(){
+
+    var a=document.getElementById("img1")
+    a.height=675
+    a.width=450
+    a.src="images/watch11.png"
+}
+function change2(){
+
+    var a=document.getElementById("img2")
+    // a.height=675
+    // a.width=450
+    a.src="images/img11.png"
+}
+
+function change3(){
+
+    var a=document.getElementById("img2")
+    // a.height=675
+    // a.width=450
+    a.src="images/img1111.png"
+}
+function change4(){
+
+    var a=document.getElementById("img3")
+    // a.height=675
+    // a.width=450
+    a.src="images/img33.png"
+}
+function change5(){
+
+    var a=document.getElementById("img3")
+    // a.height=675
+    // a.width=450
+    a.src="images/img33333.png"
+}
+function change6(){
+
+    var a=document.getElementById("img4")
+    // a.height=675
+    // a.width=450
+    a.src="images/img33.png"
+}
+function change7(){
+
+    var a=document.getElementById("img4")
+    // a.height=675
+    // a.width=450
+    a.src="images/img333.png"
+}
+function change8(){
+
+    var a=document.getElementById("img5")
+    // a.height=675
+    // a.width=450
+    a.src="images/img44.png"
+}
+function change9(){
+
+    var a=document.getElementById("img5")
+    // a.height=675
+    // a.width=450
+    a.src="images/img4444.png"
+}
+function change10(){
+
+    var a=document.getElementById("img6")
+    // a.height=675
+    // a.width=450
+    a.src="images/img3333.png"
+}
+function change11(){
+
+    var a=document.getElementById("img6")
+    // a.height=675
+    // a.width=450
+    a.src="images/img333333.png"
+}
+// function change12(){
+
+//     var a=document.getElementById("img9")
+//     // a.height=675
+//     // a.width=450
+//     a.src="images/img444.png"
+// }
+// function change13(){
+
+//     var a=document.getElementById("img9")
+//     // a.height=675
+//     // a.width=450
+//     a.src="images/img44.png"
+// }
